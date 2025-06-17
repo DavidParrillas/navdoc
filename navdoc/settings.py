8a7b8590d5458200d430b7a08f9fdc2630b44561
Carlos Martinez <emre21011992@gmail.com>
2025-06-16 23:53:49 -0400

    cambiando settings de base

--- navdoc/settings.py
+++ navdoc/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'navdoc',
-        'USER': 'postgres',
-        'PASSWORD': 'alex',
+        'USER': 'navuser',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
